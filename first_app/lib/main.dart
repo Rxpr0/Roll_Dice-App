15bc2db01d5a2d242231f047660b58e7a3e023c8
Hesham_Ak <alkatout99@gmail.com>
2026-04-02 02:19:21 -0900

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
+import 'package:first_app/dice_roll_page.dart';
+
 void main() {
-  runApp(
-     MaterialApp(
-      home: Scaffold(
-        body: GradientContainer(colors: [const Color.fromARGB(255, 175, 178, 182), const Color.fromARGB(255, 7, 89, 95)]),
+  runApp(const FirstApp());
+}
+
+class FirstApp extends StatelessWidget {
+  const FirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const seedColor = Color(0xFF07595F);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: seedColor,
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.white,
+          elevation: 0,
+          centerTitle: true,
+        ),
       ),
-    ),
-  );
+      home: const DiceRollPage(),
+    );
+  }
 }
 
